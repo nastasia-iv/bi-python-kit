d351ded18bfdbbc6ba2eb4b39b09985272255e6f
Anastasiya Ivanova <a.s.ivanova97@gmail.com>
2024-05-26 12:05:06 +0300

    Make fit_tree function private

--- custom_random_forest.py
+++ custom_random_forest.py
@@ -1,17 +1,10 @@
-# Комментарий для проверяющего
-# Я понимаю, что задача CPU-bound, значит надо использовать процессы.
-# Но я пыталась параллелить на процессах с помощью ProcessPoolExecutor,
-# и с ним у меня для fit нормально уменьшалось wall time, но росло cpu time,
-# а для predict росло cpu time, а wall time уменьшался буквально на 10-15%.
-# С потоками получилось лучше: в обоих случаях cpu +- сохраняется, а wall почти идеально падает :(
-# Айм сорри...
-
-from concurrent.futures import ThreadPoolExecutor
-
 import numpy as np
+
 from sklearn.base import BaseEstimator
 from sklearn.tree import DecisionTreeClassifier
+from concurrent.futures import ThreadPoolExecutor
 
+SEED = 42
 
 class RandomForestClassifierCustom(BaseEstimator):
     """
@@ -21,7 +14,6 @@ class RandomForestClassifierCustom(BaseEstimator):
         self, n_estimators: int = 10, max_depth: int = None,
         max_features: int = None, random_state: int = 42
     ):
-
         self.n_estimators = n_estimators
         self.max_depth = max_depth
         self.max_features = max_features
@@ -33,6 +25,35 @@ class RandomForestClassifierCustom(BaseEstimator):
 
         self.trees = []
         self.feat_ids_by_tree = []
+
+    def _fit_tree(self, tree_id: int, X: np.ndarray, y: np.ndarray) -> DecisionTreeClassifier:
+        """
+        Training a single tree in a Random Forest.
+
+        Arguments:
+            tree_id (int): Tree number.
+            X (numpy.ndarray): Feature matrix for training.
+            y (numpy.ndarray): Target variable for training.
+
+        Returns:
+            DecisionTreeClassifier: Trained decision tree.
+        """
+        tree_seed = SEED + tree_id
+        np.random.seed(tree_seed)
+
+        # Select random features for a tree
+        feature_ids = np.random.choice(self.n_features, self.max_features, replace=False)
+        self.feat_ids_by_tree.append(feature_ids)
+
+        # Select random samples to train a tree
+        sample_ids = np.random.choice(self.n_samples, self.n_samples, replace=True)
+
+        # Train a decision tree with selected features and samples
+        tree = DecisionTreeClassifier(
+            max_depth=self.max_depth, max_features=self.max_features, random_state=tree_seed
+        )
+        tree.fit(X[sample_ids][:, feature_ids], y[sample_ids])
+        return tree
 
     def fit(self, X: np.ndarray, y: np.ndarray, n_jobs: int = 1) -> 'RandomForestClassifierCustom':
         """
@@ -49,34 +70,9 @@ class RandomForestClassifierCustom(BaseEstimator):
         self.classes = sorted(np.unique(y))
         self.n_samples, self.n_features = X.shape
 
-        def fit_tree(i: int) -> DecisionTreeClassifier:
-            """
-            Training a single tree in a Random Forest.
-
-            Arguments:
-                i (int): Tree number.
-
-            Returns:
-                DecisionTreeClassifier: Trained decision tree.
-            """
-
-            # Select random features for a tree
-            feature_ids = np.random.choice(self.n_features, self.max_features, replace=False)
-            self.feat_ids_by_tree.append(feature_ids)
-
-            # Select random samples to train a tree
-            sample_ids = np.random.choice(self.n_samples, self.n_samples, replace=True)
-
-            # Train a decision tree with selected features and samples
-            tree = DecisionTreeClassifier(
-                max_depth=self.max_depth, max_features=self.max_features
-            )
-            tree.fit(X[sample_ids][:, feature_ids], y[sample_ids])
-            return tree
-
         # Parallel training of all trees
         with ThreadPoolExecutor(n_jobs) as pool:
-            self.trees = list(pool.map(fit_tree, range(self.n_estimators)))
+            self.trees = list(pool.map(lambda tree_id: self._fit_tree(tree_id, X, y), range(self.n_estimators)))
 
         return self
 
@@ -91,8 +87,7 @@ class RandomForestClassifierCustom(BaseEstimator):
         Returns:
             numpy.ndarray: A matrix of class probabilities for each sample.
         """
-        # Array of probabilities for each class
-        probas = np.zeros((self.n_samples, len(self.classes)))
+        probas = np.zeros((X.shape[0], len(self.classes)))
 
         def predict_proba_tree(tree: DecisionTreeClassifier, feature_ids: np.ndarray, X: np.ndarray) -> np.ndarray:
             """
@@ -130,7 +125,6 @@ class RandomForestClassifierCustom(BaseEstimator):
         Returns:
             numpy.ndarray: A vector of predicted classes for each sample.
         """
-        # Predict classes with the highest probability
         probas = self.predict_proba(X, n_jobs)
         predictions = np.argmax(probas, axis=1)
 
